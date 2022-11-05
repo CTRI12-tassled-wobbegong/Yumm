71274a91fd81e503b1a80b2fe6bfd6543d95b039
tiffany chau <tiffanynchau@gmail.com>
2022-11-05 11:54:08 -0500

    server structure

---
+++ server/controllers/appController.js
@@ -0,0 +1,10 @@
+const Todo = require("../../models/todoModel.js");
+
+const baseError = {
+  message: "An error occured",
+  status: 400,
+};
+
+const appController = {};
+
+module.exports = appController;
