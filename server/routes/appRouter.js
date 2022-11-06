65978fc48292ce9b01069f3bf0cf2738a4e1f443
tiffany chau <tiffanynchau@gmail.com>
2022-11-06 03:37:09 -0600

    update login scss/ update some controllers

--- server/routes/appRouter.js
+++ server/routes/appRouter.js
@@ -3,55 +3,78 @@ const router = express.Router();
 const appController = require("../controllers/appController.js");
 const userController = require("../controllers/userController.js");
 const postController = require("../controllers/postController.js");
+const commentController = require("../controllers/commentController.js");
+const makeController = require("../controllers/makeController.js");
+const friendController = require("../controllers/friendController.js");
 
+//get request for userController.getAllUsers
 router.get("/user", userController.getAllUsers, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
+//post request for userController.createUser
 router.post("/user", userController.createUser, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
+//get request for postController.getAllPosts
 router.get("/post", postController.getAllPosts, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
+//post request for postController.createPost
 router.post("/post", postController.createPost, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
-router.post("/comment", userController.getAllUsers, (req, res) => {
+//delete request for postController.deletePost
+router.delete("/post", postController.deletePost, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
-router.delete("/comment", userController.createUser, (req, res) => {
+//post request for commentController.createComment
+router.post("/comment", commentController.createComment, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
-router.post("/make", userController.getAllUsers, (req, res) => {
+/*
+//delete request for commentController.deleteComment
+router.delete("/comment", commentController.deleteComment, (req, res) => {
+  console.log(res.locals);
+  res.status(200).json(res.locals);
+});
+*/
+
+//post request for makeController.createMake
+router.post("/make", makeController.createMake, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
+/*
+//delete request for makeController.deleteMake
 router.delete("/make", userController.createUser, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
-router.post("/friend", userController.getAllUsers, (req, res) => {
+//post request for friendController.createFriend
+router.post("/friend", friendController.createFriend, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
 
+//delete request for friendController.deleteFriend
 router.delete("/friend", userController.createUser, (req, res) => {
   console.log(res.locals);
   res.status(200).json(res.locals);
 });
+*/
 
 module.exports = router;
